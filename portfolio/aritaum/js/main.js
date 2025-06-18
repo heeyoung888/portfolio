24bae0fb401f996b91c301997f5ce7b0f543496a
heeyoung888 <hyyy888@naver.com>
2025-06-18 17:22:09 +0900

    링크

--- portfolio/aritaum/js/main.js
+++ portfolio/aritaum/js/main.js
@@ -32,7 +32,7 @@ document.addEventListener("DOMContentLoaded", function () {
         },
         breakpoints: {
           768: {
-            slidesPerView: 2,
+            slidesPerView: 1.5,
           }
         }
       });
